886b3388137de517732f4fca1452211219a3e9ce
mathmartins2 <dev.sincroni@gmail.com>
2023-01-09 17:31:40 -0300

    init

--- index.js
+++ index.js
@@ -20,11 +20,16 @@ async function sendDiscordMessage(message) {
   })
 }
 
+const discordUsers = {
+  'viniciusfrancisco4': '@SUPORTEGERENCIAL#4548'
+}
+
 app.post('/', async (req, res) => {
   try {
       const { cardName, users, type, message } = req.body
+      let usersDiscord = users.map(user => discordUsers[user] || user).join(', ')
       if(type === 'ended') {
-        await sendDiscordMessage(`> the card **${cardName}** has been ended! \n > great job nossos queridos zé gotinhas **${users}**!`)
+        await sendDiscordMessage(`> the card **${cardName}** has been ended! \n > great job nossos queridos zé gotinhas **${usersDiscord}**!`)
         await prisma.user.create({
           data: {
             name: users,
@@ -33,11 +38,11 @@ app.post('/', async (req, res) => {
         return;
       }
       if(type === 'started') {
-        await sendDiscordMessage(`> the card **${cardName}** has been started! \n > good luck meus companheiros **${users}**!`)
+        await sendDiscordMessage(`> the card **${cardName}** has been started! \n > good luck meus companheiros **${usersDiscord}**!`)
         return;
       }
-      if(type === "tests") {
-        await sendDiscordMessage(`> the card **${cardName}** has been put in tests! \n > lets test them meus compatriotas **${users}**!`)
+      if(type === 'tests') {
+        await sendDiscordMessage(`> the card **${cardName}** has been put in tests! \n > lets test them meus compatriotas **${usersDiscord}**!`)
         return
       }
       if(type === 'any') {
