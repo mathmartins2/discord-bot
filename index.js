5dc91fa9d7a5bc3e57a3b00b7447a84dd22a6036
mathmartins2 <dev.sincroni@gmail.com>
2023-01-09 15:08:51 -0300

    init

---
+++ index.js
@@ -0,0 +1,14 @@
+import express from 'express'
+
+const app = express()
+
+app.use(express.json())
+
+app.post('/', (req, res) => {
+  console.log(req.body);
+  res.send('Hello World!')
+})
+
+app.listen(3000, () => {
+  console.log('Example app listening on port 3000!')
+})
